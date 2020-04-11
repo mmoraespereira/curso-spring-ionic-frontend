aaf808b881a4aad7a01e60626b0a7879c64487d9
Magdiel <magdielpereira1@hotmail.com>
2020-04-11 00:10:39 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -16,20 +16,33 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadControl: LoadingController ) {
   }
 
   ionViewDidLoad() {
  
     let categoria_id = this.navParams.get("categoria_id");
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
     .subscribe(response => {
         this.items = response["content"];
+        loader.dismiss();
     },
-    error => {});
+    error => {
+      loader.dismiss();
+    });
   }
 
   showDetail(produto_id: string){
     this.navCtrl.push("ProdutosDetailPage", {produto_id: produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadControl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
   }
 }
