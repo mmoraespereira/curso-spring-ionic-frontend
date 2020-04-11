189e4b303df242d128004ab0fb9ba3adf6f83d29
Magdiel <magdielpereira1@hotmail.com>
2020-04-11 00:00:21 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PedidoDTO } from '../../models/pedido.dto';
-import { CartItem } from '../../models/cart.item';
-import { CartService } from '../../services/domain/cart.service';
-import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { CartService } from '../../services/domain/cart.service';
 import { PedidoService } from '../../services/domain/pedido.service';
+import { CartItem } from '../../models/cart.item';
 
 @IonicPage()
 @Component({
@@ -16,50 +16,56 @@ import { PedidoService } from '../../services/domain/pedido.service';
 export class OrderConfirmationPage {
 
   pedido: PedidoDTO;
-  cartItem: CartItem[];
+  cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public cartService: CartService,
     public clienteService: ClienteService,
+    public cartService: CartService,
     public pedidoService: PedidoService) {
-    
+
     this.pedido = this.navParams.get('pedido');
   }
 
   ionViewDidLoad() {
-    this.cartItem = this.cartService.getCart().items;
+    this.cartItems = this.cartService.getCart().items;
+
     this.clienteService.findById(this.pedido.cliente.id)
-    .subscribe(response => {
-        this.cliente = response  as ClienteDTO;
-        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos'])
-    },
-    error => {
-      this.navCtrl.setRoot("HomePage");
-    });
+      .subscribe(response => {
+        this.cliente = response as ClienteDTO;
+        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
+      },
+      error => {
+        this.navCtrl.setRoot('HomePage');
+      });
   }
-  
-  private findEndereco(id: string,list: EnderecoDTO[]): EnderecoDTO{
-        let position = list.findIndex(x => x.id == id);
-        return list[position];
+
+  private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
+    let position = list.findIndex(x => x.id == id);
+    return list[position];
+  }
+
+  total() : number {
+    return this.cartService.total();
   } 
 
-  total(){
-    return this.cartService.total();
+  back() {
+    this.navCtrl.setRoot('CartPage');
   }
 
-  back(){
-    this.navCtrl.setRoot("CartPage");
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrCleanCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403) {
@@ -68,5 +74,8 @@ export class OrderConfirmationPage {
       });
   }
 
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
 }
-
